04bd1cdb35d00b4e1ba4c8b68c0af5b02b8e62ae
kristinatsk <kristinatsk@gmail.com>
2024-12-16 07:03:13 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Kristina Kostenko on 16.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
